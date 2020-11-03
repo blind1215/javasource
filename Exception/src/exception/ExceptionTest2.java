03ac621c9e1b45e434e399c1a950bf3cf7a463b4
sooyeun <blind1215@gmail.com>
2020-11-03 18:11:12 +0900

    커밋

--- Exception/src/exception/ExceptionTest2.java
+++ Exception/src/exception/ExceptionTest2.java
@@ -4,7 +4,7 @@ public class ExceptionTest2 {
 
 	public static void main(String[] args) {
 		String data1 = "100";
-		String data2 = "a100";
+		String data2 = "200";
 		
 		int value1 = Integer.parseInt(data1);
 		int value2 = Integer.parseInt(data2);//문자형을 정수형태로 변경

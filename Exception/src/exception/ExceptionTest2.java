cd58ce5d0a02590e91bc86d4e7bdb476b379a1e6
sooyeun <blind1215@gmail.com>
2020-11-03 15:02:02 +0900

    소스수정

--- Exception/src/exception/ExceptionTest2.java
+++ Exception/src/exception/ExceptionTest2.java
@@ -3,8 +3,16 @@ package exception;
 public class ExceptionTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String data1 = "100";
+		String data2 = "a100";
+		
+		int value1 = Integer.parseInt(data1);
+		int value2 = Integer.parseInt(data2);//문자형을 정수형태로 변경
+		
+		System.out.println(value1+value2);
+		
+		//실행해야 할수있는 런타임에러
+		
 	}
 
 }
